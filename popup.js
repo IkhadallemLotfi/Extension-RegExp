b73824b50f453b79cbee3b22319a1fec0f9f6b96
Lotfi <dl_ikhadallem@esi.dz>
2018-06-11 21:51:05 +0300

    mise à jour de l'icone

--- popup.js
+++ popup.js
@@ -1,5 +1,10 @@
 function call() {
     var exp = document.getElementById('regExp').value;
+    if(document.getElementById('send').checked== true){
+        chrome.browserAction.setIcon({path: "iconActive.png"});
+    }else{
+        chrome.browserAction.setIcon({path: "iconDisabled.png"});
+    }
     chrome.runtime.sendMessage({"cmd": "start","regExp":exp}, function(response) {
         console.log(response);
     });
@@ -23,7 +28,11 @@ chrome.runtime.sendMessage({"cmd": "getState"}, function(response) {
 
     document.getElementById('send').checked = !response.result;
 
-    
+    if(document.getElementById('send').checked== true){
+        chrome.browserAction.setIcon({path: "iconActive.png"});
+    }else{
+        chrome.browserAction.setIcon({path: "iconDisabled.png"});
+    }
     
 });
 
